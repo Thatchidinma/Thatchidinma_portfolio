e26826f23440d98fe577bf498a199f98c97a4c51
Chidinma <ndukubachidinma@gmail.com>
2024-08-16 18:47:59 +0100

    fixed padding in worksCard on mobile view

--- src/components/worksCard.js
+++ src/components/worksCard.js
@@ -3,7 +3,7 @@ export function WorksCard(props) {
     //iteriate over the tech props and render an element containing each one
     const tech = props.techs.map(item => <li className="border-2 border-blue-700 p-2 rounded-md hover:bg-blue-500" key={item}>{item}</li>)
     return(
-            <div className="lg:p-16 lg:m-0 grid grid-row-2 md:grid-cols-2  gap-5 lg:gap-14">
+            <div className="p-8 lg:p-16 lg:m-0 grid grid-row-2 md:grid-cols-2  gap-5 lg:gap-14">
                 <img src={props.img} alt=""/>
                 <div className="flex flex-col justify-center">
                     <h1 className="font-extrabold text-3xl text-blue-700">{props.title}</h1>

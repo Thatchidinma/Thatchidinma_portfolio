b9d94d8c75394eba8c2de4b344c9bcec9a28a4cc
Chidinma <ndukubachidinma@gmail.com>
2024-10-26 06:08:21 +0100

    made the handbuger menu for mobile fixed

--- src/components/nav.js
+++ src/components/nav.js
@@ -8,24 +8,52 @@ import { useState } from "react";
 
 export function Nav(params) {
     const [visble, setVisible] = useState(false)
+    console.log(visble)
 
     return(
-        <div >
-            <header className="bg-black/10 flex flex-col justify-center p-3 lg:pl-24 text-blue-700 ">
+        <div className=" w-full">
+            <header className=" shadow-xl bg-gray-800 flex flex-col justify-center p-3 lg:pl-24 text-blue-700 ">
                 <nav className="flex justify-between "> 
                     <Link to="/"><img src={logo} alt="" className="w-16 h-16 my-auto"/></Link>
-                    <ul className="p-4 gap-8 md:flex hidden">
-                    <li className="hover:text-blue-400"><Link to="/works">Works</Link></li>
-                    <li className="hover:text-blue-400"><Link to="/about">About</Link></li>
-                    <li className="hover:text-blue-400"><Link to="/contact">Contact me</Link></li>
+
+                    <div className={`${visble ? 'h-screen w-full flex top-0 right-0  z-50 fixed ' : ''}`}>
+                    <div className="w-1/3 bg-gray-800/60 md:hidden "></div>
+                    <ul className={`p-4 md:gap-8 md:flex transition-all ${visble ? ' w-2/3 flex flex-col h-screen shadow-2xl col-span-2 shadow-black bg-gray-800 ' : 'hidden'} `}>
+                    <li onClick={()=>setVisible(false)} className='flex item-center justify-end gap-4 p-3 cursor-pointer md:hidden'> 
+                    < IoCloseSharp className=' my-auto h-4 rotate-180' />
+                    </li>
+                    <li className='p-4 md:p-0 border-b lg:border-0 md:hidden'>
+                        <NavLink onClick={()=>setVisible(false)}  to="/">
+                            Home
+                        </NavLink>
+                    </li>
+                    <li className="p-4 md:p-0 border-b lg:border-0 hover:text-blue-400">
+                        <NavLink onClick={()=>setVisible(false)} to="/works">
+                            Works
+                            <hr className='w-2/4 border-none h-[1.5px] hidden'/>
+                        </NavLink>
+                    </li>
+                    <li className="p-4 md:p-0 border-b lg:border-0 hover:text-blue-400">
+                        <NavLink onClick={()=>setVisible(false)} to="/about">
+                            About
+                            <hr className='w-2/4 border-none h-[1.5px] hidden'/>
+                        </NavLink>
+                    </li>
+                    <li className="p-4 md:p-0 border-b lg:border-0 hover:text-blue-400">
+                        <NavLink onClick={()=>setVisible(false)} to="/contact">
+                            Contact me
+                            <hr className='w-2/4 border-none h-[1.5px] hidden'/>
+                        </NavLink>
+                    </li>
                     </ul>
+                    </div>
                     <BiMenuAltRight 
-                        className="lg:hidden text-5xl"
+                        className="md:hidden my-auto text-5xl"
                         onClick={()=>setVisible(true)}
                     />
                 </nav>
 
-                <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-gray-800 z-50 transition-all ${visble ? 'w-2/3 shadow-2xl shadow-black' : 'w-0'} `}>
+                {/* <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-gray-800 z-50 transition-all h-screen ${visble ? 'w-2/3 shadow-2xl shadow-black' : 'w-0'} `}>
                     <div className='flex flex-col text-white'>
                         <div onClick={()=>setVisible(false)} className='flex item-center gap-4 p-3 cursor-pointer'>
                             <IoCloseSharp className=' my-auto h-4 rotate-180' />
@@ -35,7 +63,7 @@ export function Nav(params) {
                         <NavLink onClick={()=>setVisible(false)} className='py-2 mx-2 pl-6 border-b' to="/about">About</NavLink>
                         <NavLink onClick={()=>setVisible(false)} className='py-2 mx-2 pl-6 border-b' to="/contact">Contact me</NavLink>
                     </div>
-                </div>
+                </div> */}
             </header>
         </div>
     )
